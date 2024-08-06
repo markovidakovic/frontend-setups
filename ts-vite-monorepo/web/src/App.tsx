aa8942116bd45d15958575b7a03b222eb618b889
Marko Vidaković <markovidakovic29@gmail.com>
2024-08-06 11:57:47 +0200

    -

---
+++ ts-vite-monorepo/web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Account } from 'common/types';
+
+export default function App() {
+	let usr: Account = {
+		id: 1,
+	};
+	console.log(usr);
+	return (
+		<div>
+			<p>webapp</p>
+		</div>
+	);
+}
